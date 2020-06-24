dbe33448134a188411b64e054525bca108a18729
Prince Amit <princeamitlali@gmail.com>
2020-06-24 21:55:55 +0530

    Add files via upload

---
+++ nameSerializer.py
@@ -0,0 +1,7 @@
+import os
+path = 'Potato___Late_blight'
+files = os.listdir(path)
+
+
+for index, file in enumerate(files):
+    os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), '.jpg'])))
